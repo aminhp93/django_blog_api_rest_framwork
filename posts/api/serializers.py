8131b3cbaf9ee0691bb2a06e6ccccc746d9686fd
aminhp93 <minhpn.org.ec@gmail.com>
2017-01-13 20:10:14 -0800

    serializer method field

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,4 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+	ModelSerializer, 
+	HyperlinkedIdentityField, 
+	SerializerMethodField
+	)
 
 from posts.models import Post
 
@@ -11,23 +15,64 @@ class PostCreateUpdateSerializer(ModelSerializer):
 			'publish',
 		]
 
+post_detail_url = HyperlinkedIdentityField(
+		view_name = 'posts-api:detail',
+		lookup_field = 'slug',
+		)
+
 class PostDetailSerializer(ModelSerializer):
+	url = post_detail_url
+	user = SerializerMethodField()
+	image = SerializerMethodField()
+	markdown = SerializerMethodField()
+
+	def get_user(self, obj):
+		return str(obj.user.username)
+
+	def get_image(self, obj):
+		try:
+			image = obj.image.path
+		except:
+			image = None
+		return image
+
+	def get_markdown(self, obj):
+		return obj.get_markdown()
+
 	class Meta:
 		model = Post
 		fields = [
+			'image',
+			'user',
+			'url',
 			'id',
 			'title',
 			'slug',
 			'content',
 			'publish',
+			'markdown',
 		]
 
 class PostListSerializer(ModelSerializer):
+	url = post_detail_url
+
+	delete_url = HyperlinkedIdentityField(
+		view_name = "posts-api:delete",
+		lookup_field = "slug"
+		)
+	user = SerializerMethodField()
+
+	def get_user(self, obj):
+		return str(obj.user.username)
+
 	class Meta:
 		model = Post
 		fields = [
+			'url',
+			'user',
 			'title',
-			'slug',
+			# 'slug',
 			'content',
 			'publish',
+			'delete_url',
 		]
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,12 +1,31 @@
+from django.db.models import Q
+
+from rest_framework.filters import (
+	SearchFilter,
+	OrderingFilter,
+	)
+
 from rest_framework.generics import (
 	CreateAPIView,
 	ListAPIView, 
 	RetrieveAPIView,
 	UpdateAPIView,
 	DestroyAPIView,
+	RetrieveUpdateAPIView,
+	)
+
+from .pagination import PostLimitOffsetPagination, PostPageNumberPagination
+
+from rest_framework.permissions import (
+	AllowAny,
+	IsAuthenticated,
+	IsAdminUser,
+	IsAuthenticatedOrReadOnly,
 	)
 
 from posts.models import Post
+
+from .permissions import IsOwnerOrReadOnly
 
 from .serializers import (
 	PostListSerializer, 
@@ -17,10 +36,33 @@ from .serializers import (
 class PostCreateAPIView(CreateAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostCreateUpdateSerializer
+	permission_classes = [IsAuthenticated]
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
+
 
 class PostListAPIView(ListAPIView):
-	queryset = Post.objects.all()
+	# queryset = Post.objects.all()
 	serializer_class = PostListSerializer
+	filter_backends = [SearchFilter, OrderingFilter]
+	search_fields = ["title", "content", "user__first_name"]
+	pagination_class = PostPageNumberPagination
+
+	def get_queryset(self, *args, **kwargs):
+		# queryset_list = super().get_queryset(*args, **kwargs)
+		queryset_list = Post.objects.all()
+		query = self.request.GET.get("q")
+		if query:
+			queryset_list = queryset_list.filter(
+				Q(title__icontains=query) |
+				Q(content__icontains=query) |
+				Q(user__first_name__icontains=query) |
+				Q(user__last_name__icontains=query) 
+				).distinct()
+		return queryset_list
+
+
 
 class PostDetailAPIView(RetrieveAPIView):
 	queryset = Post.objects.all()
@@ -28,12 +70,18 @@ class PostDetailAPIView(RetrieveAPIView):
 	lookup_field = "slug"
 	# lookup_url_kwarg = "abc"
 
-class PostUpdateAPIView(UpdateAPIView):
+class PostUpdateAPIView(RetrieveUpdateAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostCreateUpdateSerializer
 	lookup_field = "slug"
+	permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+	def perform_update(self, serializer):
+		serializer.save(user=self.request.user)
 
 class PostDestroyAPIView(DestroyAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostDetailSerializer
 	lookup_field = "slug"
+	permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
